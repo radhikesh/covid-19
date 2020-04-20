62fec23268742c51e60a497fc25e384c43a48022
radhikesh <rradhikesh@gmail.com>
2020-04-19 20:19:26 -0500

    added cron file

--- cron/src/get_data.R
+++ cron/src/get_data.R
@@ -3,21 +3,33 @@ get_data <- function(){
   
   devtools::install_github("RamiKrispin/coronavirus", force = T,upgrade = F)
   
-  library(pacman)
+  #library(pacman)
   
-  p_load(
-    "devtools",
-    "coronavirus",
-    "flexdashboard",
-    "tidyverse",
-    "leaflet",
-    "htmltools",
-    "dplyr",
-    "DT",
-    "plotly",
-    "data.table",
-    "lubridate"
-  )
+  # p_load(
+  #   "devtools",
+  #   "coronavirus",
+  #   "flexdashboard",
+  #   "tidyverse",
+  #   "leaflet",
+  #   "htmltools",
+  #   "dplyr",
+  #   "DT",
+  #   "plotly",
+  #   "data.table",
+  #   "lubridate"
+  # )
+  
+  library(devtools)
+  library(coronavirus)
+  library(flexdashboard)
+  library(tidyverse)
+  library(leaflet)
+  library(htmltools)
+  library(dplyr)
+  library(DT)
+  library(plotly)
+  library(data.table)
+  library(lubridate)
   
   data("coronavirus")
   
